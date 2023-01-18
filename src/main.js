4b1ab0f83e54ec03faacbfb645a1729ba21a741f
gitfarag <react.codework@gmail.com>
2023-01-18 05:12:25 +0200

    add fa-icons fix-:class-input

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 const app = createApp(App)
 const pinia = createPinia()
+
+library.add(faTrash)
 app.use(pinia)
 app.use(router)
+app.component('fa', FontAwesomeIcon)
 app.mount('#pro')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,10 @@ import Hero from '@/components/Hero.vue'
 import { globalStore } from '@/store/globals.store';
 import { onMounted } from 'vue';
 import {getTodos, deleteTodos} from '@/controller/todos.controlloer'
+import { ref } from 'vue';
 
 const store = globalStore()
+const done = ref(false)
 
 onMounted(async()=>{
     const res = await  getTodos()
@@ -23,15 +25,36 @@ const deleteHandler = async (id)=>{
     <NavBar/>
     <Hero/>
     <div class="container">
-        <div class="row d-flex flex-column align-items-center">
-            <div class="col-6 d-flex justify-content-between mt-3" v-for="todo in store.todos">
+        <div class="row flex-column justify-content-center align-items-center">
+            <div class="col-11 col-md-6 mt-3 item" :class="done && `bg-success`" v-for="todo in store.todos">
                 <p>
                     {{ 
                     todo.name
                 }} :)
                 </p>
-                <button class="btn btn-outline-dark" @click="deleteHandler(todo.id)">X</button>
+                <fa icon="fa-solid fa-trash" @click="deleteHandler(todo.id)"></fa>
+
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+.item{
+    background-color: rgba(0, 0, 0, 0.267);
+    color: white;
+    border-radius: 10px;
+    align-items: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+}
+.item p {
+    margin: auto 0;
+}
+.fa-trash:hover{
+    cursor: pointer;
+    color: rgb(231, 35, 35);
+    transition: 0.3s;
+}
+</style>
